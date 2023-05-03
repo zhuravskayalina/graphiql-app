15d4fa2f8b4ad5eed3367325ddd48279a094de3c
Angelina <zhuravskayalina@gmail.com>
2023-05-03 11:47:26 +0200

    feat: change application name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>GraphQl App</title>
+        <title>GraphiQL App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
