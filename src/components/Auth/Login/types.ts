e37150aff230b7c5b83cff597d660bb20c3f9961
Diana_Bobrova <dziana_babrova@epam.com>
2023-05-04 23:08:10 +0300

    refactor: use useForm for login

--- src/components/Auth/Login/types.ts
+++ src/components/Auth/Login/types.ts
@@ -3,3 +3,8 @@ import { Dispatch, SetStateAction } from 'react';
 export type LoginProps = {
   activeRegisterOption: Dispatch<SetStateAction<boolean | null>>;
 };
+
+export type LoginFormsFields = {
+  email: string;
+  password: string;
+};
