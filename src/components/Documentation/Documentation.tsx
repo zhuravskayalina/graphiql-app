721405423d72404ef858ae334f86a62d2bc42277
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-05-06 16:33:16 +0200

    feat: add generated types

--- src/components/Documentation/Documentation.tsx
+++ src/components/Documentation/Documentation.tsx
@@ -1,13 +1,46 @@
+import { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { INTROSPECTION } from '@/pages/api/introspection';
 import styles from './Documentation.module.scss';
+import { IntrospectionQuery } from '../../generatedTypes/IntrospectionQuery';
 
 const Documentation = () => {
-  const { loading, error, data } = useQuery(INTROSPECTION);
+  const [currentType, setCurrentType] = useState<string>('Schema');
+  const [previousType, setPreviousType] = useState<string | null>(null);
+  const { loading, error, data } = useQuery<IntrospectionQuery>(INTROSPECTION);
   console.log(data);
+
   return (
     <div className={styles.wrapper}>
-      <h3>Documentation</h3>
+      {data && currentType === 'Schema' ? (
+        <div>
+          <h3>Documentation</h3>
+          <p>
+            {
+              data.__schema.types.find((type) => type.name === data.__schema.__typename)
+                ?.description
+            }
+          </p>
+          <p>Root Types</p>
+          {data.__schema.queryType ? (
+            <div>
+              query:<p>{data.__schema.queryType.name}</p>
+            </div>
+          ) : null}
+          {data.__schema.mutationType ? (
+            <div>
+              mutation:<p>`${data.__schema.mutationType.name}`</p>
+            </div>
+          ) : null}
+          {data.__schema.subscriptionType ? (
+            <div>
+              subscription:<p>`${data.__schema.subscriptionType.name}`</p>
+            </div>
+          ) : null}
+        </div>
+      ) : (
+        <h3>`${currentType}`</h3>
+      )}
     </div>
   );
 };
