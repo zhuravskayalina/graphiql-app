d4dec48d4b11e1fbb25f7aee560a6f5402fae15f
Angelina <zhuravskayalina@gmail.com>
2023-05-09 19:40:36 +0200

    feat: add styles and adaptive layout to editor page

--- src/components/Response/Response.tsx
+++ src/components/Response/Response.tsx
@@ -2,8 +2,19 @@ import styles from './Response.module.scss';
 
 const Response = () => {
   return (
-    <div className={styles.wrapper}>
-      <h3>Response</h3>
+    <div className={styles.response}>
+      <h3 className={styles.response__heading}>Response</h3>
+      <div className={styles.response__text}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, dolorum eveniet impedit
+        neque quam qui quisquam quos. Alias architecto assumenda commodi delectus doloribus ducimus
+        ea impedit ipsam iste magnam modi, nisi officiis perspiciatis, porro quia repellendus sint
+        vero? Amet corporis cumque debitis dignissimos, excepturi hic laudantium modi, non odit
+        officia, qui quo quod ratione ullam voluptatibus. Accusamus adipisci alias amet animi,
+        asperiores beatae consequuntur dicta, eaque, eum facere incidunt mollitia odit perferendis
+        possimus quis quo quod voluptates! Ad, assumenda consectetur dignissimos dolores ea et
+        fugiat iure minus, molestiae neque non nostrum quas quos saepe, sapiente sunt temporibus vel
+        veritatis vitae!
+      </div>
     </div>
   );
 };
--- src/pages/graphiql.tsx
+++ src/pages/graphiql.tsx
@@ -1,20 +1,38 @@
+import { clsx } from 'clsx';
 import Documentation from '@/components/Documentation/Documentation';
 import Request from '@/components/Request/Request';
 import Response from '@/components/Response/Response';
 import Variables from '@/components/Variables/Variables';
 import styles from '../styles/Graphiql.module.scss';
+import { useMediaQuery } from 'react-responsive';
+import Image from 'next/image';
+import docIcon from '@/assets/images/icons/book.svg';
+import { useEffect, useState } from 'react';
 
 const Graphiql = () => {
+  const isTablet = useMediaQuery({ maxWidth: 1100 });
+  const [tabletScreen, setTabletScreen] = useState(false);
+  const [openDoc, setOpenDoc] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTabletScreen(isTablet);
+  }, [isTablet]);
+
   return (
     <div className={styles.main}>
-      <div className={styles.main__documentation}>
-        <Documentation />
+      <div className={clsx(styles.main__documentation, styles.section)}>
+        {tabletScreen && (
+          <button className={styles.main__openButton} onClick={() => setOpenDoc(true)}>
+            <Image src={docIcon} alt="open documentation" />
+          </button>
+        )}
+        <Documentation isOpen={openDoc} isTablet={tabletScreen} setOpenDoc={setOpenDoc} />
       </div>
-      <div className={styles.main__request}>
+      <div className={clsx(styles.main__request, styles.section)}>
         <Request />
         <Variables />
       </div>
-      <div className={styles.main__response}>
+      <div className={clsx(styles.main__response, styles.section)}>
         <Response />
       </div>
     </div>
