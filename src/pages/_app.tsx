17f51d82864b0dca1f1e87786ae3d0ea363e18f0
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-05-09 09:10:30 +0200

    fix: conflict, remove dependencies to dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout/Layout';
-<<<<<<< HEAD
+import '@/i18n/i18n';
 import '@/styles/globals.scss';
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
   cache: new InMemoryCache(),
 });
-=======
-import '@/i18n/i18n';
->>>>>>> origin/develop
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
