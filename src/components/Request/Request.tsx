6a21402b6447264627eee9e79bf35b8ac492849d
Angelina <zhuravskayalina@gmail.com>
2023-05-09 21:48:22 +0200

    feat: add translation to main page

--- src/components/Request/Request.tsx
+++ src/components/Request/Request.tsx
@@ -4,8 +4,10 @@ import Editor from '@monaco-editor/react';
 import { editor } from 'monaco-editor';
 import runIcon from '../../assets/images/icons/run.svg';
 import Image from 'next/image';
+import { useTranslation } from 'react-i18next';
 
 const Request = () => {
+  const { t } = useTranslation();
   const editorRef = useRef<editor.IStandaloneCodeEditor>();
 
   const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
@@ -15,10 +17,9 @@ const Request = () => {
   return (
     <div className={styles.request}>
       <div className={styles.request__header}>
-        <p className={styles.request__heading}>Operation</p>
+        <p className={styles.request__heading}>{t('operation')}</p>
         <button className={styles.runBtn}>
           <Image src={runIcon} alt="run" />
-          <span>Send</span>
         </button>
       </div>
       <section className={styles.editor}>
