6bd4cb9e8ea14edf9e5fc806372259691f869b8b
Angelina <zhuravskayalina@gmail.com>
2023-05-14 20:50:40 +0200

    feat: add animated sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,30 @@ import Link from 'next/link';
 import HeaderButton from '@/components/Header/HeaderButton/HeaderButton';
 import { paths } from '@/enums/routerPaths';
 import { auth, logout } from '@/services/authService';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import getNotificationType, { sendNotification } from '@/services/firebaseNotificationService';
 import LanguageToggle from './LanguageToggle/LanguageToggle';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { clsx } from 'clsx';
 
 const Header = () => {
   const { t, i18n } = useTranslation();
   const [user, loading] = useAuthState(auth);
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
 
   const handleLanguageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const language = e.target.checked ? e.currentTarget?.value : 'default';
@@ -27,14 +42,13 @@ const Header = () => {
   };
 
   return (
-    <header className={styles.header}>
+    <header className={clsx(styles.header, styles.sticky, isSticky && styles.onScroll)}>
       <Link href={paths.welcome} className={styles.header__logo}>
-        <Image src={logoImg} alt="logo" />
+        <Image src={logoImg} alt="logo" className={styles.header__logoIcon} />
       </Link>
       <p className={styles.header__appName}>GraphiQL App</p>
       {!loading && (
         <div className={styles.header__buttons}>
-          <LanguageToggle changeLanguage={handleLanguageChange} />
           {user ? (
             <>
               <HeaderButton link={paths.main} title={t('backToMain')} />
@@ -46,6 +60,7 @@ const Header = () => {
               <HeaderButton link={paths.signUp} title={t('signUp')} />
             </>
           )}
+          <LanguageToggle changeLanguage={handleLanguageChange} />
         </div>
       )}
     </header>
