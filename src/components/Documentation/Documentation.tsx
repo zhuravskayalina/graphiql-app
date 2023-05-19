f1c16780e7b19f8166fa9c1df75e94398a4a59a9
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-05-19 13:37:48 +0200

    fix: change scroll styles in documentation block

--- src/components/Documentation/Documentation.tsx
+++ src/components/Documentation/Documentation.tsx
@@ -44,9 +44,9 @@ const Documentation = () => {
   const typeObject = data?.__schema.types.find((type) => type.name === currentType);
 
   return (
-    <>
-      <h3 className={styles.title}>{t('documentationTitle')}</h3>
-      <p className={styles.subtitle}>{t('apiDescription')}</p>
+    <div className={styles.documentation}>
+      <h3 className={styles.documentation__title}>{t('documentationTitle')}</h3>
+      <p className={styles.documentation__subtitle}>{t('apiDescription')}</p>
       {isError && <p>{t('failed')}</p>}
       {data && currentType === typePath[0] ? (
         <Schema schema={data.__schema} changeType={changeType} />
@@ -54,33 +54,35 @@ const Documentation = () => {
         data && (
           <>
             <TypePath path={typePath} changeType={changeType} />
-            {typeObject?.description ? (
-              <p className={styles.typeDescription}>{typeObject?.description}</p>
-            ) : null}
-            {typeObject?.fields
-              ? typeObject?.fields?.map((field) => (
-                  <div className={styles.box} key={field.name}>
-                    <div className={styles.box__title}>
-                      <span>{field.name}</span>
-                      <Arguments args={field.args} changeType={changeType} />
-                      {':'}&nbsp;
-                      <Type type={field.type} changeType={changeType} />
+            <div className={styles.types}>
+              {typeObject?.description ? (
+                <p className={styles.types__description}>{typeObject?.description}</p>
+              ) : null}
+              {typeObject?.fields
+                ? typeObject?.fields?.map((field) => (
+                    <div className={styles.box} key={field.name}>
+                      <div className={styles.box__title}>
+                        <span>{field.name}</span>
+                        <Arguments args={field.args} changeType={changeType} />
+                        {':'}&nbsp;
+                        <Type type={field.type} changeType={changeType} />
+                      </div>
+                      <p className={styles.box__description}>{field.description}</p>
                     </div>
-                    <p className={styles.box__description}>{field.description}</p>
-                  </div>
-                ))
-              : typeObject?.inputFields?.map((input) => (
-                  <div className={clsx(styles.box, styles.box__filter)} key={input.name}>
-                    <p>{input.description}</p>
-                    <span>{input.name}</span>
-                    {':'}&nbsp;
-                    <Type type={input.type} changeType={changeType} />
-                  </div>
-                ))}
+                  ))
+                : typeObject?.inputFields?.map((input) => (
+                    <div className={clsx(styles.box, styles.box__filter)} key={input.name}>
+                      <p>{input.description}</p>
+                      <span>{input.name}</span>
+                      {':'}&nbsp;
+                      <Type type={input.type} changeType={changeType} />
+                    </div>
+                  ))}
+            </div>
           </>
         )
       )}
-    </>
+    </div>
   );
 };
 
