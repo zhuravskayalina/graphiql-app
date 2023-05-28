0f63b30665c1f01b59ef799d1e349584cba6bba1
Diana_Bobrova <dziana_babrova@epam.com>
2023-05-28 15:38:07 +0300

    feat: remove header on 404 page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,8 +3,11 @@ import Head from 'next/head';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import styles from './Layout.module.scss';
+import { useRouter } from 'next/router';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
+  const { pathname } = useRouter();
+
   return (
     <>
       <Head>
@@ -12,7 +15,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      {pathname !== '/404' && <Header />}
       <main className={styles.main}>{children}</main>
       <Footer />
     </>
